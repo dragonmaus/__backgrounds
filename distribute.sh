25ba12e6da8f2e14ce43a24e89a9702a779a3bb7
David Wildasin <dragonmaus@posteo.net>
2025-04-19 20:31:52 -0400

    Update management scripts

--- distribute.sh
+++ distribute.sh
@@ -2,10 +2,10 @@
 
 find new -type f | while read f
 do
-	eval $(gm identify -format 'w=%w\nh=%h' "$f" 2> /dev/null)
+	eval $(gm identify -format 'w=%w;h=%h;' "$f" 2> /dev/null)
 	r=$(ratio $w $h | sed 's/(1)$/(1:1)/' | tr : _)
-	g=$r/$w'x'$h/${f#*/}
-	if [ "$f" != "$g" ]
+	g=${r}/${w}x${h}/${f#*/}
+	if test "x$f" != "x$g"
 	then
 		mkdir -p -v "$(dirname "$g")"
 		mv -f -v "$f" "$g"
